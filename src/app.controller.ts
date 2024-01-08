20077351db798cbfc9f393cb0ab2743e38a17270
范綉莉 <shakira.fang@secom.com.tw>
2024-01-08 16:46:36 +0800

    add delete update find method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './modules/user/user.service';
+import { User } from './modules/user/models/user.entity';
 
 @Controller()
 export class AppController {
@@ -14,5 +15,30 @@ export class AppController {
       password: '123456',
       account: 'admin',
     });
+  }
+
+  @Get('/delete')
+  async deleteUser(): Promise<boolean> {
+    return await this.userService.deleteUser(
+      '6c5a1906-4829-47b6-b95f-827df3346836',
+    );
+  }
+
+  @Get('/update')
+  async updateUser(): Promise<boolean> {
+    return await this.userService.updateUser(
+      '55ce1855-3bb4-4096-9c29-a9dc8e86b7cd',
+      {
+        name: '莉莉update管理員',
+        desc: '管理員22',
+      },
+    );
+  }
+
+  @Get('/find')
+  async findUser(): Promise<User> {
+    return await this.userService.findUser(
+      '55ce1855-3bb4-4096-9c29-a9dc8e86b7cd',
+    );
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, DeepPartial } from 'typeorm';
 import { User } from './models/user.entity';
 
+//寫完service一定要在use.module的providers跟exports中引入。
+//這樣app.controller才能調用這個UserService。
 @Injectable()
 //依賴注入的對象
 export class UserService {
@@ -19,5 +21,33 @@ export class UserService {
       return true;
     }
     return false;
+  }
+
+  //刪除一個用戶
+  async deleteUser(id: string): Promise<boolean> {
+    const res = await this.UserRepository.delete(id);
+    if (res.affected > 0) {
+      return true;
+    }
+    return false;
+  }
+
+  //更改一個用戶
+  async updateUser(id: string, entity: DeepPartial<User>): Promise<boolean> {
+    const res = await this.UserRepository.update(id, entity);
+    if (res.affected > 0) {
+      return true;
+    }
+    return false;
+  }
+
+  //查詢一個用戶
+  async findUser(id: string): Promise<User> {
+    const res = await this.UserRepository.findOne({
+      where: {
+        id,
+      },
+    });
+    return res;
   }
 }
